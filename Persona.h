618c3656531d0835a21507d71c26b146a4952586
Walexis07 <walexsom369@gmail.com>
2026-05-20 11:01:28 -0500

    Se crea el archivo Persona.h  y se define declara sus atrivutos y metodos,

---
+++ Persona.h
@@ -0,0 +1,72 @@
+#ifndef PERSONA_H
+
+// Se definen los métodos y atributos para llevar a cabo el desarrollo del proyecto
+
+#define PERSONA_H
+
+// Declaraciones de métodos y atributos de la clase Persona
+
+#include "Librerias.h"
+
+class Persona {
+    private:
+        string nombre;
+        int edad;
+        int ID;
+
+    public:        
+        Persona(string nombre, int edad, int ID);       // Constructor para inicializar los atributos de la clase Persona.
+            
+        virtual ~Persona() = default;                   // Destructor virtual para realizar polimorfismo.
+
+        // Setters y getters para los atributos de la clase Persona.
+
+        /**
+         * @brief Establece el nombre que representa a una persona en el sistema bibliotecario.
+         * @param nombre Variable nombre que se establecerá como atributo nombre.
+         * @return No retorna nada.
+         */
+        void setNombre(string nombre);
+
+        /**
+         * @brief Establece la edad que tiene una persona en el sistema bibliotecario.
+         * @param edad Variable edad que se establecerá como atributo edad.
+         * @return No retorna nada.
+         */
+        void setEdad(int edad);
+
+        /**
+         * @brief Establece el documento de identidad que representa a una persona en el sistema bibliotecario.
+         * @param ID Variable documento de identidad que se establecerá como atributo ID.
+         * @return No retorna nada.
+         */
+        void setID(int ID);
+
+        /**
+         * @brief Obtiene el nombre que representa a una persona en el sistema bibliotecario.
+         * @return El nombre de la persona.
+         */        
+        string getNombre() const;
+
+        /**
+         * @brief Obtiene la edad que tiene una persona en el sistema bibliotecario.
+         * @return La edad de la persona.
+         */
+        int getEdad() const;
+
+        /**
+         * @brief Obtiene el documento de identidad que representa a una persona en el sistema bibliotecario.
+         * @return El documento de identidad de la persona.
+         */
+        int getID() const;
+
+        /**
+         * @brief Método virtual puro para mostrar información de Persona en el sistema bibliotecario.
+         * @return No retorna nada.
+         */
+        virtual void showInformation() const = 0;
+};
+
+#endif
+
+// Fin de las declaraciones de librerias y elementos globales
