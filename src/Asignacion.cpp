f8c9a62b18b0118509eba869e216d20661d46c49
Walexis07 <walexsom369@gmail.com>
2026-05-20 16:54:51 -0500

    SE crea el archivo Asingacion.cpp con la implementacion de los metodos.

---
+++ src/Asignacion.cpp
@@ -0,0 +1,70 @@
+#include "Asignacion.h"
+
+Asignacion::Asignacion(string codigoDeAsignacion, Usuario* usuarioAsignado, Bibliotecario* bibliotecarioAsignado, Libro* libroAsignado) {
+    this->codigoDeAsignacion = codigoDeAsignacion;
+    this->usuarioAsignado = usuarioAsignado;
+    this->bibliotecarioAsignado = bibliotecarioAsignado;
+    this->libroAsignado = libroAsignado;
+}
+
+Asignacion::Asignacion(string codigoDeAsignacion, string estadoDeAsignacion, Usuario* usuarioAsignado, Bibliotecario* bibliotecarioAsignado, Libro* libroAsignado) {
+    this->codigoDeAsignacion = codigoDeAsignacion;
+    this->estadoDeAsignacion = estadoDeAsignacion;
+    this->usuarioAsignado = usuarioAsignado;
+    this->bibliotecarioAsignado = bibliotecarioAsignado;
+    this->libroAsignado = libroAsignado;
+}
+
+void Asignacion::setcodigoDeAsignacion(string codigoDeAsignacion) {
+    this->codigoDeAsignacion = codigoDeAsignacion;
+}
+
+void Asignacion::setEstadoDeAsignacion(string estadoDeAsignacion) {
+    this->estadoDeAsignacion = estadoDeAsignacion;
+}
+
+void Asignacion::setUsuarioAsignado(Usuario* usuarioAsignado) {
+    this->usuarioAsignado = usuarioAsignado;
+}
+
+void Asignacion::setLibroAsignado(Libro* libroAsignado) {
+    this->libroAsignado = libroAsignado;
+}
+
+void Asignacion::setBibliotecarioAsignado(Bibliotecario* bibliotecarioAsignado) {
+    this->bibliotecarioAsignado = bibliotecarioAsignado;
+}
+
+string Asignacion::getCodigoAsignacion() const {
+    return this->codigoDeAsignacion;
+}
+
+string Asignacion::getEstadoDeAsignacion() const {
+    if (estadoDeAsignacion == "Activo") {
+        return "Activo";
+    } else if (estadoDeAsignacion == "Inactivo") {
+        return "Inactivo";
+    } else {
+        return "Estado Desconocido";
+    }
+}
+
+Usuario* Asignacion::getUsuarioAsignado() const {
+    return this->usuarioAsignado;
+}
+
+Libro* Asignacion::getLibroAsignado() const {
+    return this->libroAsignado;
+}
+
+Bibliotecario* Asignacion::getBibliotecarioAsignado() const {
+    return this->bibliotecarioAsignado;
+}
+
+void Asignacion::mostrarInformacion() const { 
+    std::cout << "Asignacion [" << getEstadoDeAsignacion() << "] -> Usuario: " << getUsuarioAsignado()->getNombre() 
+              << " | Codigo: " << getUsuarioAsignado()->getCodigoUsuario() << " | Libro: " << getLibroAsignado()->getTitulo()
+              << " | Codigo: " << getLibroAsignado()->getCodigoLibro() << " | Bibliotecario Responsable: " 
+              << getBibliotecarioAsignado()->getNombre() << " | Codigo: " << getBibliotecarioAsignado()->getCodigoEmpleado() 
+              << "\n" << std::endl;
+}
